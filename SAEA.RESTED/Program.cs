374d91d6f7e58e6865599930bcda663b4d51c748
wenli <wenguoli_520@qq.com>
2019-03-06 11:06:06 +0800

    add new

---
+++ SAEA.RESTED/Program.cs
@@ -0,0 +1,27 @@
+﻿using SAEA.MVC;
+using System;
+using System.Diagnostics;
+
+namespace SAEA.RESTED
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "SAEA.RESTED";
+
+            SAEAMvcApplicationConfig mvcApplicationConfig = SAEAMvcApplicationConfigBuilder.Read();
+
+            SAEAMvcApplication mvcApplication = new SAEAMvcApplication(mvcApplicationConfig);            
+
+            mvcApplication.Start();
+
+            Console.WriteLine("SAEA.RESTED 服务已启动，回车结束服务.");
+
+            Process.Start("http://localhost:39654/");
+
+            Console.ReadLine();
+
+        }
+    }
+}
