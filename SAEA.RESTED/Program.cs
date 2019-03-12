9cff4b0af1c1d8663fba7fecbdde11183d43b687
wenli <wenguoli_520@qq.com>
2019-03-12 17:39:52 +0800

    add service test

--- SAEA.RESTED/Program.cs
+++ SAEA.RESTED/Program.cs
@@ -1,13 +1,41 @@
-﻿using SAEA.Common;
+﻿/****************************************************************************
+*项目名称：SAEA.RESTED
+*CLR 版本：4.0.30319.42000
+*机器名称：WENLI-PC
+*命名空间：SAEA.RESTED
+*类 名 称：Program
+*版 本 号：V1.0.0.0
+*创建人： yswenli
+*电子邮箱：wenguoli_520@qq.com
+*创建时间：2019/3/12 10:31:06
+*描述：
+*=====================================================================
+*修改时间：2019/3/12 10:31:06
+*修 改 人： yswenli
+*版 本 号： V1.0.0.0
+*描    述：
+*****************************************************************************/
+using SAEA.Common;
 using SAEA.MVC;
+using SAEA.RESTED.Libs;
 using System;
 using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.ServiceProcess;
 
 namespace SAEA.RESTED
 {
     class Program
     {
-        static void Main(string[] args)
+
+        private static readonly string Name = "SAEA.RESTED Service";
+        private static readonly string Display = "SAEA.RESTED Service";
+        private static readonly string Description = "这是SAEA.RESTED 服务，关闭此服务将无法访问SAEA.RESTED。";
+        private static readonly string FilePath = Assembly.GetExecutingAssembly().Location;
+
+
+        static void ConsoleStart()
         {
             Console.Title = "SAEA.RESTED";
 
@@ -15,18 +43,83 @@ namespace SAEA.RESTED
 
             SAEAMvcApplicationConfig mvcApplicationConfig = SAEAMvcApplicationConfigBuilder.Read();
 
-            SAEAMvcApplication mvcApplication = new SAEAMvcApplication(mvcApplicationConfig);            
+            SAEAMvcApplication mvcApplication = new SAEAMvcApplication(mvcApplicationConfig);
 
             mvcApplication.Start();
 
-            ConsoleHelper.WriteLine("SAEA.RESTED 服务已启动，请在浏览器中输入 http://localhost:39654/");
+            ConsoleHelper.WriteLine("SAEA.RESTED 服务已启动，请在浏览器中输入 \r\n\t http://localhost:39654/");
 
             ConsoleHelper.WriteLine("回车结束服务");
 
             Process.Start("http://localhost:39654/");
 
             Console.ReadLine();
+        }
+
+       static SAEARESTEDService _service;
+
+        /// <summary>
+        /// 启动入口
+        /// </summary>
+        /// <param name="args"></param>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            try
+            {
+                #region 参数相关功能
+
+                if (args.Length != 0)
+                {
+                    switch (args[0].ToUpper())
+                    {
+                        case "/I":
+                            WinServiceHelper.InstallAndStart(FilePath, Name, Display, Description);
+                            return;
+                        case "/U":
+                            WinServiceHelper.Unstall(Name);
+                            return;
+                        default:
+                            Console.WriteLine("args:");
+                            Console.WriteLine("\t/i\t\t 安装服务");
+                            Console.WriteLine("\t/u\t\t 卸载服务");
+                            return;
+                    }
+                }
+                else
+                {
+                    try
+                    {
+#if DEBUG
+                        ConsoleStart();
+#else
+
+                        _service = new SAEARESTEDService();
+                        _service.Run();
+#endif
+
+                    }
+                    catch (Exception ex)
+                    {
+                        SystemLoger("启动失败，原因：" + ex.Message + ex.Source);
+                    }
+                }
+
+                #endregion
+            }
+            catch (Exception ex)
+            {
+                SystemLoger("启动失败，原因：" + ex.Message + ex.Source);
+            }
 
         }
+
+        private static readonly string SysLogPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"SAEA.RESTED.Service.log");
+
+        private static void SystemLoger(string message)
+        {
+            File.AppendAllText(SysLogPath, DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + message + "\r\n");
+        }
     }
+
 }
---
+++ SAEA.RESTED/SAEARESTEDService.cs
@@ -0,0 +1,39 @@
+﻿using SAEA.MVC;
+using System.ServiceProcess;
+
+namespace SAEA.RESTED
+{
+    partial class SAEARESTEDService : ServiceBase
+    {
+        SAEAMvcApplication _mvcApplication;
+
+        public SAEARESTEDService()
+        {
+            InitializeComponent();
+
+            var mvcApplicationConfig = SAEAMvcApplicationConfigBuilder.Read();
+
+            _mvcApplication = new SAEAMvcApplication(mvcApplicationConfig);
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            _mvcApplication.Start();
+        }
+
+        protected override void OnStop()
+        {
+            _mvcApplication.Stop();
+        }
+
+        public void Run()
+        {
+            ServiceBase[] ServicesToRun =
+            {
+                new SAEARESTEDService()
+            };
+            Run(ServicesToRun);
+        }
+
+    }
+}
